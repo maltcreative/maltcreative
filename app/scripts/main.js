2a2ccd53e3a1fc7ed0dfb5b4d58c8e5b75d2bcc4
Nick Roberts <nick@maltcreative.com.au>
2016-07-14 11:08:04 +1000

    Only run packery when needed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,19 @@
-var pckry = new Packery( '.grid', {
+var mq = matchMedia('(min-width: 400px)');
+var pckry;
+var pckryGrid = document.querySelector('.grid');
+var pckryOptions = {
   itemSelector: '.item',
   gutter: 0
+};
+
+if (mq.matches) {
+  pckry = new Packery(pckryGrid, pckryOptions);
+}
+
+mq.addListener(function(mql) {
+  if (mql.matches) {
+    pckry = new Packery(pckryGrid, pckryOptions);
+  } else {
+    pckry.destroy();
+  }
 });
